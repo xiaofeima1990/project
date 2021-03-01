a4b91524777929aa40c252d87b6fdc24b7e2583e
unknown <mgxglen@gmail.com>
2021-02-28 19:03:15 -0500

    finish the year selection part

--- web_spider/fdic/fdic_sum_report.py
+++ web_spider/fdic/fdic_sum_report.py
@@ -31,6 +31,7 @@ from datetime import date, timedelta,datetime
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException,TimeoutException
 import selenium.common.exceptions as S_exceptions
@@ -43,7 +44,12 @@ import pandas as pd
 page_load_flag = ".tdHeaderSOD"
 
 
-
+def choose_selections(driver, element_id, select_year):
+    driver.find_element(By.ID, element_id).click()
+    el = driver.find_element(By.ID, element_id) 
+    for option in el.find_elements_by_tag_name('option'):
+        if select_year in option.text :
+            option.click()
 
 def open_page(driver,url):
     # only firefox is OK !!! 
@@ -76,3 +82,23 @@ if __name__ == '__main__':
     driver = webdriver.Firefox(firefox_options=options,firefox_profile=profile,executable_path=driver_path)        
         
     driver=open_page(driver,base_url)
+    
+    
+    
+    ### navigate to the HHI part 
+    pro_forma=driver.find_element_by_xpath("//font[contains(.,'Pro-Forma')]")
+    pro_forma.click()
+    
+    '''
+    Report Creation:
+        1.GEO area
+        
+    '''
+    ### 1. geo area
+    geo_selection = driver.find_element_by_css_selector("#divtdTabProForma tr:nth-child(2) > td > label")
+    geo_selection.click()
+    ### 2. select year 
+    year_selection = Select(driver.find_element_by_id('PFMSADepositDate'))
+    year_selection.select_by_value("2001")
+    
+    
