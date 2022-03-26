733bcde6ad67dfc71378f46b34ca925e759c2972
Guoxuan <mgxglen@gmail.com>
2022-03-25 20:42:50 -0400

    udpate for taobao webspider

--- web_spider/land auction taobao/find_absinfo_linkv2.py
+++ web_spider/land auction taobao/find_absinfo_linkv2.py
@@ -26,7 +26,7 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException,TimeoutException,StaleElementReferenceException
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.options import Options
 import pandas as pd
 import sqlite3
 import urllib
@@ -209,8 +209,8 @@ def next_page(driver,page_count,flag):
 
 if __name__ == '__main__':
 
-    file_path="E:\\Dropbox\\academic\\11_work_dataset\\justice auction\\link\\house\\"
-    driver_path="E:\\github\\Project\\web_spider\\land auction taobao\\geckodriver.exe"    
+    file_path="D:\\Dropbox\\academic\\11_work_dataset\\justice auction\\link\\house\\"
+    driver_path= current_path+"\\geckodriver.exe"    
 #    con = sqlite3.connect("E:\\justice_auction.sqlite")
     
 #     this even requires gbk decoding encoding!!! to convert str to url
@@ -226,10 +226,12 @@ if __name__ == '__main__':
     henan= ['郑州','开封','洛阳','平顶山','安阳','鹤壁','新乡','焦作','济源','濮阳','许昌','漯河','三门峡','南阳','商丘','信阳','周口','驻马店']
     guangxi = ['南宁','柳州','桂林','梧州','北海','防城港','钦州','贵港','玉林','百色','贺州','河池','来宾','崇左']
     sichuan = ['成都','自贡','攀枝花','泸州','德阳','绵阳','广元','遂宁','内江','乐山','南充','眉山','宜宾','广安','达州','雅安','巴中','资阳','阿坝','甘孜','凉山']
-    city_name = ['铜川','宝鸡','咸阳','渭南','延安','汉中','榆林','安康','商洛']
+    shaanxi = ['铜川','宝鸡','咸阳','渭南','延安','汉中','榆林','安康','商洛']
     shanxi2 = ['太原','大同','阳泉','长治','晋城','朔州','晋中','运城','忻州','临汾','吕梁']
     
     heilongjiang = ['哈尔滨','齐齐哈尔','鸡西','鹤岗','双鸭山','大庆','伊春','佳木斯','七台河','牡丹江','黑河','绥化','大兴安岭']
+    
+    city_name = ['南京','苏州']
     
 #    ele=input("input city name: ")
     flag_auction_time=input("input auction time choice: 1- first time, 2- second time, 3- 1+2, : ")
@@ -246,22 +248,25 @@ if __name__ == '__main__':
     # 1 - Allow all images
     # 2 - Block all images
     # 3 - Block 3rd party images 
-    profile.set_preference("permissions.default.image", 2)
+    # profile.set_preference("permissions.default.image", 2)
     # driver=webdriver.Firefox(firefox_profile=profile)
-    options = FirefoxOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Firefox(firefox_options=options,firefox_profile=profile,executable_path=driver_path)
+    options = Options()
+    # options.headless = True
+    driver = webdriver.Firefox(options=options,firefox_profile=profile,executable_path=driver_path)
 
+    '''
+    need to deal with the login problem
     
+    '''
     # PhantomJS is depresiated but I am still going to use it 
-    driver = webdriver.PhantomJS() # depresiated 
+    # driver = webdriver.PhantomJS() # depresiated 
     
     for ele in city_name:
         print("-----------------------------------")
         print("city "+ele+ " begins")
         print("-----------------------------------")
         error_flag = 0
-        year_list=['2014','2015','2016','2017']
+        year_list=['2020','2021','2022']
         #    for ele in city_name:
         elee=ele.encode("gbk")
         elee=urllib.parse.quote(elee)
@@ -290,6 +295,9 @@ if __name__ == '__main__':
             time_url="&auction_start_from="+start_time.strftime("%Y-%m-%d")+"&auction_start_to="+end_time.strftime("%Y-%m-%d")
             start_url=base_url+time_url
             driver=open_page(driver,start_url)
+            '''
+            manually login
+            '''
             if error_flag == 1:
                 if y != error_year:
                     continue
